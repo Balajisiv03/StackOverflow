ca0988dffc26161b98c18119569dd29ab02aa332
Balajisiv03 <115578788+Balajisiv03@users.noreply.github.com>
2023-08-11 10:41:16 +0530

    First Commit

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import search from '../../assets/search.svg';
+import Avatar from '../../components/Avatar/Avatar';
+import './navbar.css';
+
+const Navbar = () => {
+  var User= null
+  return (
+    <nav>
+      <div className="navbar">
+          <Link to ="/" className="nav-item nav-logo">
+            <img src={logo} alt="logo" width="150" />
+          </Link>
+          <Link to ="/" className="nav-item nav-btn">About</Link>
+          <Link to ="/" className="nav-item nav-btn">Products</Link>
+          <Link to ="/" className="nav-item nav-btn">For Teams</Link>
+          <form>
+            <input type="text" placeholder='...Search'/>
+            <img src={search} alt="Search" width="18" className="search-icon"></img>
+          </form>
+          { User === null ?
+             <Link to = "/Auth" className="nav-item nav-links">Login</Link>: 
+             <>
+              <Avatar backgroundColor="#009dff" px="10px" py="7px" borderRadius="50%"><Link to="/User" style={{color:"white" ,textDecoration: "none"}} >M</Link></Avatar>
+              <button className="nav-item nav-links">Log out</button>
+             </>
+          }
+
+      </div>
+    </nav>
+    )
+} 
+  
+
+export default Navbar
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import './Auth.css';
+import logo from '../../assets/logo.png'
+import AboutAuth from './AboutAuth'
+
+const Auth = () => {
+  const[IsSignUp,setIsSignUp]=useState(false)
+  const handleswitch=()=>{
+    setIsSignUp(!IsSignUp)
+  }
+  return (
+     <section class="auth-section">
+       {IsSignUp && <AboutAuth/>}
+        <div class="auth-container">
+          {!IsSignUp && <img src={logo} alt="stackoverflow-icon" className="login-logo"/>}
+          <form>
+            {
+              IsSignUp && (
+              <label htmlFor="name">
+                  <h4>Display Name</h4>
+                  <input type="text" id="name" name="name"/>
+              </label>)
+            }
+            <label htmlFor="email">
+              <h4>Email</h4>
+              <input type="email" id="email" name="email"/>
+            </label>
+            <label htmlFor="password">
+              <div style={{display: "flex",justifyContent: "space-between"}}>
+                <h4>Password</h4>
+                {!IsSignUp && <p style={{color:"#007ac6" ,fontSize: "13px"}}>Forget Password</p>}
+              </div>
+              <input type="password" id="password" name="password"/>  
+              {IsSignUp && (<p style={{color:"#666767",fontSize: "13px"}}>Passwords must contain atleast eight<br /> characters,including atleast 1 letter and 1<br/> number.</p>
+              )}
+              </label>  
+              {IsSignUp && (
+                <label htmlFor="check">
+                  <input type="checkbox" id="check"/>
+                  <p style={{fontSize: "13px"}}>Opt-in to reeive occasionsal,<br/>product updates,user research invitations,<br/>company announcementa,digests</p>
+                </label>
+              )}
+              {IsSignUp && (
+                <p style={{color:"#666767",fontSize: "13px"}}>
+                  By clicking "Sign Up", you agree to our 
+                  <span style={{color:"#007ac6"}}> terms of<br/>service</span>,
+                  <span style={{color:"#007ac6"}}> privacy policy</span>,
+                  <span style={{color:"#007ac6"}}> cookie policy</span>
+                </p>)}
+              <button type="submit" className="auth-btn">{IsSignUp?"Sign Up":"Log in"}</button>
+          </form>
+          <p>
+            {IsSignUp?"Already have an account":"Don't have an account"}
+            <button type="submit" className="handle-switch-btn" onClick={handleswitch}>{IsSignUp?"Login":"Sign Up"}</button>
+          </p>
+        </div>
+     </section>
+  )
+}
+
+export default Auth
